4ec9d768d3c8c327c91fbd9c5aaaab242778432c
just-boris <just-boris@hotmail.com>
2018-08-12 23:04:58 +0200

    fix styles

--- src/components/ItemView/index.tsx
+++ src/components/ItemView/index.tsx
@@ -4,17 +4,22 @@ import { Item } from "../../interfaces";
 import styled from "../../styled";
 import Panel from "../Panel";
 
+const EditLink = styled(Link)({});
+
 const Title = styled("h2")({
   borderBottom: "1px solid #ddd",
   display: "flex",
-  paddingBottom: "3px"
+  paddingBottom: "3px",
+  [`& > ${EditLink}`]: {
+    marginLeft: "auto"
+  }
 });
 
 const ItemView: React.SFC<{ item: Item }> = ({ item }) => {
   return (
     <Panel>
       <Title>
-        {item.title} <Link to="./edit">Edit</Link>
+        {item.title} <EditLink to="./edit">Edit</EditLink>
       </Title>
       <b>Stats</b>
       <ul>
